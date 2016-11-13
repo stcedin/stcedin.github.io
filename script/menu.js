62e1fb4d99f9339cf83bdf4a10457c067e6af423
stcedin <stcampbell.misc@blueyonder.co.uk>
2016-11-13 15:41:39 +0000

    Refinements to the paged

---
+++ script/menu.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+document.write('\
+\
+		<ul class="menu">\
+  		<li><a href="home.html">Home</a></li>\
+  		<li><a href="componentsearch.html">Component Search</a></li>\
+  		<li><a href="translate.html">Translate</a></li>\
+  		<li><a href="calculator.html">Calculator</a></li>\
+	</ul>\
+\
+');
